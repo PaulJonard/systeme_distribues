166263f9a3dbeaa602f694e58ef0a0fde8f005f1
PaulJonard <Paul.Jonard@etu.univ-savoie.fr>
2022-09-24 13:27:55 +0200

    Question 6

--- src/fr/univ_smb/iae/mtii/jonardp/vehicules/VehiculeApp.java
+++ src/fr/univ_smb/iae/mtii/jonardp/vehicules/VehiculeApp.java
@@ -3,8 +3,21 @@ package fr.univ_smb.iae.mtii.jonardp.vehicules;
 public class VehiculeApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Vehicule v1 = new Voiture("Peugeot", 5);
+		Vehicule v2 = new Camion("Volvo", 1);
+		Vehicule v3 = new Camion("Mercedes", 2);
+		
+		v1.setCouleur("rouge");
+		v1.setNum_imat("AA-001-AA");
+		v1.setVitesse_inst(110);
+		
+		v2.setCouleur("grise");
+		v2.setNum_imat("AA-002-AA");
+		v2.setVitesse_inst(90);
+		
+		v3.setCouleur("blanche");
+		v3.setNum_imat("AA-003-AA");
+		v2.setVitesse_inst(80);
 	}
 
 }
